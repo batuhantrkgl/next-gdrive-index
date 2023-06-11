deb97162b6b603d70233169e5d8569d03581e660
Batuhan Türkoğlu <76158371+batuhantrkgl@users.noreply.github.com>
2023-06-11 21:43:03 +0300

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Client ID are safe to expose
   client_id:
-    "126409166174-l2unckghm8d5m1gp3deu0uaps5son64f.apps.googleusercontent.com",
+    "929017811152-84htpp4gn5p1a0740hj4cpuor1u9nd3o.apps.googleusercontent.com",
   // Client secret and refresh token are encrypted
   // Using encryption key
   // https://drive.mbaharip.com/gudora-setup
   client_secret:
-    "54f0505d11a8fe04d22ec642cdde4728:a6f911c70b0305a51160a2133a9bd2ada3c120567857002311f17bf29cc69e07d7f1a9ca20fd119d684191aee6e3866a",
+    "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",
   refresh_token:
-    "209b4d2ff44bf877ce0636ddc81cdc9a:4a40e172ba5d6c1ee8daf50389841bd2c9d1153163196da0dab63c07b6f305bedafaa209a9e6adbb8d2377c74ba98cf818b6e73b8f57098686ed0b89bec93b0186047ccaab8804310a2ab4e46069d1cac8c322f0f1e206808ac62c18639610be07559eebad904905ba185cae8ba9fbe9",
+    "1//04k3KOR2gN14-CgYIARAAGAQSNwF-L9IriWjvQF2q_jhPxOmNu4AJJqO5jZ5qWCLnobxQ5SlQYklD1vqNQMMPfWcK8JAfB8ezkbg"
 
   files: {
     // How many files to show per page
@@ -20,7 +20,7 @@ module.exports = {
     // Or use folder id to use a specific folder as starting point
     // TODO: Change when final
     // rootFolder: "root",
-    rootFolder: "1KgPV6QB1GYT8fmn2uTfbtr9rDXqcRR0j", // Test folder
+    rootFolder: "root", // Test folder
     // Limit breadcrumb to specific depth
     // 0 = Unlimited
     // 1 = Only show current folder
